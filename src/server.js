4cb0f961b56b76dac4774b0b67400d0af2294f80
Carlos Daniel <dev.cdfaria@gmail.com>
2021-04-05 13:05:24 -0300

    Aula 2

--- src/server.js
+++ src/server.js
@@ -6,11 +6,15 @@ const server = express()
 // Importa as rotas
 const routes = require('./routes')
 
+// Usando template engine
 server.set('view engine', 'ejs')
 
 // Passa para o server o que ele irá usar
 // Habilitar arquivos estáticos
 server.use(express.static("public"))
+
+// Utilizar o req.body
+server.use(express.urlencoded({ extended: true }))
 
 // Rotas
 server.use(routes)
